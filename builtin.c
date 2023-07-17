1219e479886992479370b3d07a5f7a9010e7558f
ezra <ezrayeneneh1992@gmail.com>
2023-07-17 03:33:29 -0700

    did betty

--- builtin.c
+++ builtin.c
@@ -1,23 +1,26 @@
 #include "terminal.h"
 
-int find_builtin(info_t *info) {
-    int i, built_in_ret = -1;
-    builtin_table builtintbl[] = {
-    {"exit", ourexit},
-    {"env", ourenv},
-    {"cd", ourcd},
-    {"setenv", oursetenv},
-    {"unsetenv", ourunsetenv},
-    {"alias", ouralias},
-    {NULL, NULL}
-};
+int find_builtin(info_t *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", ourexit},
+		{"env", ourenv},
+		{"cd", ourcd},
+		{"setenv", oursetenv},
+		{"unsetenv", ourunsetenv},
+		{"alias", ouralias},
+		{NULL, NULL}
+	};
 
-    for (i = 0; builtintbl[i].type; i++) {
-        if (_strcmp(info->argv[0], builtintbl[i].type) == 0) {
-            built_in_ret = builtintbl[i].func(info);
-            break;
-        }
-    }
+	for (i = 0; builtintbl[i].type; i++)
+	{
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			built_in_ret = builtintbl[i].func(info);
+			break;
+		}
+	}
 
-    return built_in_ret;
+	return (built_in_ret);
 }
